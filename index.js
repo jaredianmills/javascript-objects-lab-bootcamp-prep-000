69d1e6942121fcd98222453f5e9c4a6a0f79ca0c
jaredianmills <jaredianmills@gmail.com>
2018-04-16 19:03:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 var recipes = {eggs: 2, butter: '1 tablespoon', salt: '1 pinch', cheese: '1/4 cup', bacon: '2 ounces'};
 
-function updateObjectWithKeyAndValue(object, key, value) {}
+function updateObjectWithKeyAndValue(object, key, value) {
+  object[key] =  value
+}
