49da24b4cefee79c10bbe9abaabbdd28152d1a91
jaredianmills <jaredianmills@gmail.com>
2018-04-16 19:01:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-var recipes = {eggs: 2, butter: '1 tablespoon', salt: '1 pinch', cheese: '1/4 cup' }
+var recipes = {eggs: 2, butter: '1 tablespoon', salt: '1 pinch', cheese: '1/4 cup', bacon: '2 ounces' }
