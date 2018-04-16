f0cfe3e3eaa061d9f5d02bd21bf6db8daecc82c3
jaredianmills <jaredianmills@gmail.com>
2018-04-16 19:07:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,4 +3,6 @@ var recipes = {eggs: 2, butter: '1 tablespoon', salt: '1 pinch', cheese: '1/4 cu
 function updateObjectWithKeyAndValue(object, key, value) {
   var updatedObject = Object.assign({}, object, {[key]: value});
   return updatedObject;
-}
+};
+
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value)
