78ce057f154a076f04a2a713e2b8358965832f05
jaredianmills <jaredianmills@gmail.com>
2018-04-16 19:14:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,4 +14,9 @@ function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj[key];
   return newObj;
+};
+
+function destructivelyDeleteFromObjectByKey(object, key) {
+  delete object[key];
+  return object;
 }
